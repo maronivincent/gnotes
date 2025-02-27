0243485b3cf9317d0b9031c521ffff19f7a045b8
3kla <guillaume.colombani947@gmail.com>
2025-02-27 09:10:14 +0100

    Add files via upload

---
+++ sio2E4/src/main/java/org/openjfx/sio2E4/controller/AddStudentController.java
@@ -0,0 +1,28 @@
+package org.openjfx.sio2E4.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddStudentController {
+
+    @FXML
+    private TextField nameField;
+
+
+
+    @FXML
+    private void saveStudent() {
+        String name = nameField.getText();
+
+        if (name.isEmpty()) {
+            System.out.println("Tous les champs doivent être remplis !");
+            return;
+        }
+
+        // Logic to save the Student (e.g., write to a database or a file)
+        System.out.println("Eleves ajouté : " + name);
+
+        // Optionally clear fields after saving
+        nameField.clear();
+    }
+}
